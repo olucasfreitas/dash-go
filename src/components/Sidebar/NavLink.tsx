d07cc1d02f74aacae8689d05c729be625d167d6e
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2023-10-24 11:37:30 -0300

    added clickable links to sidebar

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,17 +1,24 @@
-import { Icon, Link, LinkProps, Text } from "@chakra-ui/react";
+import { Icon, Link as ChakraLink, LinkProps, Text } from "@chakra-ui/react";
+import Link from "next/link";
 
 interface NavLinkProps extends LinkProps {
   icon: React.ElementType;
+  href: string;
   children: string;
 }
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" {...rest}>
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWeight="medium">
-        {children}
-      </Text>
-    </Link>
+    <ChakraLink display="flex" {...rest}>
+      <Link
+        href={href}
+        style={{ display: "flex", textDecoration: "none", color: "inherit" }}
+      >
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWeight="medium">
+          {children}
+        </Text>
+      </Link>
+    </ChakraLink>
   );
 }
