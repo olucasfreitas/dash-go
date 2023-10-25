9b89573932a2ca5406366aac16803253c566ff24
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2023-10-24 23:03:47 -0300

    added color to active link

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,5 +1,6 @@
 import { Icon, Link as ChakraLink, LinkProps, Text } from "@chakra-ui/react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 interface NavLinkProps extends LinkProps {
   icon: React.ElementType;
@@ -8,10 +9,26 @@ interface NavLinkProps extends LinkProps {
 }
 
 export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
+  const { asPath } = useRouter();
+
+  const isActive = asPath.includes(href);
+
   return (
     <Link href={href} style={{ display: "flex", color: "inherit" }} passHref>
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWeight="medium">
+      <Icon
+        as={icon}
+        fontSize="20"
+        color={{
+          base: isActive ? "pink.400" : "white",
+        }}
+      />
+      <Text
+        ml="4"
+        fontWeight="medium"
+        color={{
+          base: isActive ? "pink.400" : "white",
+        }}
+      >
         {children}
       </Text>
     </Link>
